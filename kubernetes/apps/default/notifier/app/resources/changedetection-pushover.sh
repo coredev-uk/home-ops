a24550caaff740f15d0ca948b9b57abf9e418ee8
Core <core@coredev.uk>
2026-05-19 22:39:37 +0100

    chore: update priority

--- kubernetes/apps/default/notifier/app/resources/changedetection-pushover.sh
+++ kubernetes/apps/default/notifier/app/resources/changedetection-pushover.sh
@@ -21,7 +21,7 @@ function notify() {
     url=$(_jq '.url // .watch_url // "https://changes.hera.ac"')
 
     apprise -vv --title "${title}" --body "${message}" --input-format html \
-        "${APPRISE_CHANGE_DETECTION_PUSHOVER_URL}?url=${url}&url_title=View Watch&priority=low&format=html"
+        "${APPRISE_CHANGE_DETECTION_PUSHOVER_URL}?url=${url}&url_title=View Watch&priority=normal&format=html"
 }
 
 function main() {

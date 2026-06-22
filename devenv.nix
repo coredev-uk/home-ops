771eea8c3527cec1dfec26cc9a6baf40c8c16278
Core <core@coredev.uk>
2026-06-22 20:50:08 +0100

    chore: change notifier to chaski

--- devenv.nix
+++ devenv.nix
@@ -1,7 +1,30 @@
 { config, pkgs, ... }:
 
+let
+  chaski = pkgs.buildGoModule {
+    pname = "chaski";
+    version = "0.2.0";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "home-operations";
+      repo = "chaski";
+      rev = "0.2.0";
+      hash = "sha256-u87DBy6lEzPsQ2O/6fExJqAOK6r5basNz1l79W7iQU4=";
+    };
+
+    # The locked devenv nixpkgs currently has Go 1.26.2 while chaski's go.mod
+    # requires the next patch release. The code builds cleanly with 1.26.2.
+    postPatch = ''
+      substituteInPlace go.mod --replace-fail "go 1.26.4" "go 1.26.2"
+    '';
+
+    vendorHash = "sha256-tVzSYSZ8eMETrML4ReLZx6P+OcoXbilfYnuFH4VUOyc=";
+    subPackages = [ "cmd/chaski" ];
+  };
+in
 {
   packages = with pkgs; [
+    chaski
     just
     minijinja
     oxfmt
